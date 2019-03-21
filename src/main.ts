9e42f29b9af1124739f746bf3a5772940b418c94
Denis Angelov <de.angelov.bg@gmail.com>
2019-03-21 07:20:16 +0200

    Added user module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
-const port = 4000;
+const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- src/shared/validation.pipe.ts
+++ src/shared/validation.pipe.ts
@@ -12,7 +12,7 @@ import { plainToClass } from 'class-transformer';
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
   async transform(value: any, metadata: ArgumentMetadata) {
-    if (value instanceof Object && this.isEmpty(object)) {
+    if (value instanceof Object && this.isEmpty(value)) {
       throw new HttpException(
         'Validation failed: No body submitted',
         HttpStatus.BAD_REQUEST,
