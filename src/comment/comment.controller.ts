53fa5eb73b130fc0bff88e1dd300f56f167fb7fe
de.angelov.bg@gmail.com <de.angelov.bg@gmail.com>
2019-03-29 07:47:25 +0200

    comment service functionality

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Param, UseGuards, UsePipes } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards, UsePipes, Post, ValidationPipe, Body, Delete } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { AuthGuard } from 'src/shared/auth.guard';
+import { CommentDto } from './comment.dto';
+import { User } from 'src/shared/user.decorator';
 
 @Controller('api/comment')
 export class CommentController {
@@ -20,9 +22,20 @@ export class CommentController {
   @Post('idea/:id')
   @UseGuards(new AuthGuard())
   @UsePipes(new ValidationPipe())
-  createComment(@Param('id') idea:string ){
-
-  
+  createComment(@Param('id') idea:string, @User('id') user: string, @Body() data: CommentDto ){
+    return this.commentService.create(idea, user, data);
   }
+
+  @Get(':id')
+  showComment(@Param('id') id: string){
+      return this.commentService.show(id);
+  }
+
+  @Delete(':id')
+  @UseGuards(new AuthGuard())
+  destoyComment(@Param('id') id: string, @User('id') user: string){
+
+  }
+
 
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentEntity } from './comment.entity';
+import { Repository } from 'typeorm';
+import { IdeaEntity } from 'src/idea/idea.entity';
+import { UserEntity } from 'src/user/user.entity';
+import { CommentDto } from './comment.dto';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(CommentEntity) private commentRepository: Repository<CommentEntity>, 
+    @InjectRepository(IdeaEntity) private ideaRepository: Repository<IdeaEntity>,
+    @InjectRepository(UserEntity) private userRepository: Repository<UserEntity> 
+  ){}
+
+  async showByIdea(id: string){
+    const idea = await this.ideaRepository.findOne({
+      where: {id},
+      relations: ['comments', 'comments.autor', 'comments.idea'];
+    });
+
+    return idea.comments;
+
+  }
+
+  async showByUser(id: string){
+    const comments = await this.commentRepository.find({
+      where: { author: {id }},
+      relations: ['Author'],
+    });
+
+
+    return comments;
+  }
+
+  async show(id: string){
+    const comment = await this.commentRepository.findOne({ where: {id}, relations: ['autor', 'idea']});
+    return comment;
+  }
+
+  async create(ideaId: string, userId: string, data: CommentDto){
+    const idea = await this.ideaRepository.findOne({where: {id: ideaId} });
+    const user = await this.userRepository.findOne({where: {id: userId} });
+
+    const comment = await this.commentRepository.create({
+      ...data,
+      idea,
+      author: user
+    });
+
+    await this.commentRepository.save(comment);
+    return comment;
+  }
+
+  async destroy(id: string, userId: string){
+    const comment = await this.commentRepository.findOne({
+      where: {id},
+      relations: ['author', 'idea'],
+    });
+
+    if(comment.author.id !== userId){
+      throw new HttpException('You do not own this comment',HttpStatus.UNAUTHORIZED);
+    }
+
+    await this.commentRepository.remove(comment);
+    return comment;
+  }
+}
